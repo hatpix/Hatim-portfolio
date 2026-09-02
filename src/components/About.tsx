91e9fa1f7d47f713852b9b669aa6c5b3209d7717
Hatim <Hatimnathwara12@gmail.com>
2026-09-02 16:39:42 +0300

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,8 +8,8 @@ const About = () => {
         <p className="para">
           Graphic Designer with 5+ years of experience across travel, digital marketing, IT, and
           event-management industries. Skilled in branding, social media, print, website design (Wix),
-          and UI design for app and web products in Figma. Comfortable owning projects end-to-end —
-          from concept to client-ready delivery — while collaborating closely with marketing, product,
+          and UI design for app and web products in Figma. Comfortable owning projects end-to-end
+          from concept to client-ready delivery while collaborating closely with marketing, product,
           and development teams to meet deadlines without compromising quality.
         </p>
       </div>
